6088be2173dcc8db0cf4c907cf994bf103a2e775
pisey <loempiseydec25@gmail.com>
2022-09-13 16:36:44 +0700

    config storybook

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,23 +1,23 @@
 // .storybook/main.js
-
 const path = require("path");
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  "core": {
-        builder: "webpack5"
-    },
+//   "core": {
+//         builder: "webpack5"
+//     },
   "stories": [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../stories/**/*.stories.tsx"
   ],
   "addons": [
-    
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    '@storybook/addon-controls',
-    '@storybook/addon-postcss',
+    "@storybook/addon-interactions",
+    'storybook-css-modules-preset',
+    'storybook-addon-next-router',
+    // Add PostCSS into addons for compiling tailwind below
     {
         name: '@storybook/addon-postcss',
         options: {
@@ -27,18 +27,19 @@ module.exports = {
         },
       },
   ],
+  //End PostCSS
   framework: "@storybook/react",
   core: {
     "builder": "@storybook/builder-webpack5"
   },
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: false,
-    reactDocgenTypescriptOptions: {
-       shouldExtractLiteralValuesFromEnum: true,
-       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
- },
+//   typescript: {
+//     check: false,
+//     checkOptions: {},
+//     reactDocgen: false,
+//     reactDocgenTypescriptOptions: {
+//        shouldExtractLiteralValuesFromEnum: true,
+//        propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+//     },
+//  },
 }
 
