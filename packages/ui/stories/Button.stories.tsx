c395b4a1c4dff86b930dc577e8fb320de4d86587
Loem Pisey <loempiseydec25@gmail.com>
2023-02-28 15:56:47 +0700

    fixed error build

--- packages/ui/stories/Button.stories.tsx
+++ packages/ui/stories/Button.stories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Story } from '@storybook/react';
-import Button, {ButtonProps} from '../components/Button';
+// import { Story } from '@storybook/react';
+// // import Button, {ButtonProps} from '../components/Button';
 
-export default {
-  title: 'Button',
-  component: Button,
-};
+// export default {
+//   title: 'Button',
+//   component: Button,
+// };
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+// const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = { text: 'Button' };
+// export const Primary = Template.bind({});
+// Primary.args = { text: 'Button' };
 
 
